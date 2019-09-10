04fbda7354f7fb8add49759310483b14eabb93b3
KrutNA <krutna@pm.me>
2019-09-11 01:55:00 +0300

    Changed variant to 824714

--- Main.java
+++ Main.java
@@ -3,88 +3,107 @@ import java.util.stream.IntStream;
 
 public class Main {
     
-    public static void main(String[] args) {    
+    public static void main(String[] args) {
+	
 	// Setup constants for first array
-	final int firstArraySize = 18;
-	final int firstArrayMaxValue = 19;
+	final int firstArraySize = 10;
 	// Setup constants for second array
-	final int secondArraySize = 11;
-	final float secondArrayMin = -15.0F;
-	final float secondArrayMax = 9.0F;
+	final int secondArraySize = 10;
+	final double secondArrayMin = -5.0;
+	final double secondArrayMax = 9.0;
 	// Setup constants for third array
-	final int firstFunctionExpectedValue = 15;
+	final int firstFunctionExpectedValue = 11;
 	final int[] secondFunctionExpectedArray = {
-	    2, 7, 8, 10, 11,
-	    12, 14, 16, 19
+	    7, 15, 17, 21, 23
 	};
 	
 	// Arrays initialization
-	int[] first = new int[firstArraySize];
-	float[] second = new float[secondArraySize];
+	long[] first = new long[firstArraySize];
+	double[] second = new double[secondArraySize];
 	double[][] third = new double[firstArraySize][secondArraySize];
 	
 	// Setup values of first array
 	for (int i = 0; i < firstArraySize; ++i) {
-	    first[i] = firstArrayMaxValue - i;
+	    first[i] = setupFirstArray(i);
 	}
 	// Setup values of second array
 	for (int i = 0; i < secondArraySize; ++i) {
-	    second[i] = (float)
+	    second[i] = (double)
 		(Math.random() *
-		 (secondArrayMax + secondArrayMin) -
+		 (secondArrayMax - secondArrayMin) +
 		 secondArrayMin);
 	    
 	}
 	// Setup values of third array
 	for (int i = 0; i < firstArraySize; ++i) {
 	    for (int j = 0; j < secondArraySize; ++j) {
-		final int value = first[i];
-		final float result = second[j];
+		final long value = first[i];
+		final double result = second[j];
 		if (value == firstFunctionExpectedValue) {
 		    
-		    third[i][j] = firstFunction(result);
+		    third[i][j] = executeFirstFunction(result);
 		    
 		} else if (IntStream.of(secondFunctionExpectedArray)
 			   .anyMatch(val -> val == value)) {
 		    
-		    third[i][j] = secondFunction(result);
+		    third[i][j] = executeSecondFunction(result);
 		    
 		} else {
 		    
-		    third[i][j] = thirdFunction(result);
+		    third[i][j] = executeThirdFunction(result);
 		    
 		}
 	    }
 	}
 	// Print third function to the screen
-	// printToScreen(third, firstArraySize, secondArraySize);
-	printFullToScreen(third,
+	// printTwoDimensionalArrayToScreen(third, firstArraySize, secondArraySize);
+        printTwoDimensionalArrayToScreenWithTable(third,
 			  first, firstArraySize,
 			  second, secondArraySize);
     }
-    
-    static double firstFunction(final double value) {
-	return Math.tan(Math.cos(powAnyValue(value,
-					     (2.0/3.0) * value)));
+
+    static long setupFirstArray
+	(final int value) {
+	
+	return (value << 1) + 5; 
     }
     
-    static double secondFunction(final double value) {
-	double dimension = 0.75 / (0.25 - Math.asin((value - 3.0) / 24.0));
-	return powAnyValue((2.0 + powAnyValue(Math.tan(value),
-					    Math.cos(value))) / 2.0,
-			   dimension);
+    static double executeFirstFunction
+	(final double value) {
+
+	return Math.atan
+	    (Math.cos
+	     (Math.log
+	      (powAnyValue
+	       (Math.sin(value), 2.0D))));
     }
     
-    static double thirdFunction(final double value) {
-	return (1.0/3.0) /
-	    (powAnyValue((4.0 /
-			  Math.tan((powAnyValue(value,
-						(value + 1.0) / value)))),
-			 3.0) - 4.0);
+    static double executeSecondFunction
+	(final double value) {
+
+	return Math.tan
+	    (Math.cos
+	     (Math.asin
+	      ((value + 2.0D) / 14.0D)));
     }
     
-    static double powAnyValue(double value, final double dimension) {
-	boolean isNegative = value < 0;
+    static double executeThirdFunction
+	(final double value) {
+	
+	return Math.asin
+	    (Math.sin
+	     (Math.asin
+	      (Math.sin
+	       (Math.log
+		(powAnyValue
+		 (2.0D / Math.abs(value), value))))));
+    }
+    
+    static double powAnyValue
+	(double value,
+	 final double dimension) {
+	
+	boolean isNegative = value < 0 && Math.pow(value, dimension) == Double.NaN;
 	if (isNegative) {
 	    value *= -1.0;
 	}
@@ -95,10 +114,11 @@ public class Main {
 	return result;
     }
 
-    private static void
-	printToScreen(final double[][] arr,
-		      final int columns,
-		      final int lines) {
+    private static void printTwoDimensionalArrayToScreen
+	(final double[][] arr,
+	 final int columns,
+	 final int lines) {
+	
         for (int i = 0; i < columns; ++i) {
             for (int j = 0; j < lines; ++j) {
                 System.out.printf("%13.4e", arr[i][j]);
@@ -106,12 +126,13 @@ public class Main {
             System.out.println();
         }
     }
-    private static void
-	printFullToScreen(final double[][] arr,
-			  final int[] first,
-			  final int lines,
-			  final float[] second,
-			  final int columns) {
+    private static void printTwoDimensionalArrayToScreenWithTable
+	(final double[][] arr,
+	 final long[] first,
+	 final int lines,
+	 final double[] second,
+	 final int columns) {
+	
 	System.out.printf("    \u2503");
 	for (int i = 0; i < columns; ++i) {
 	    System.out.printf("%13.4e", second[i]);
